e18db024c257a0badf1f2925dbb8e39116b7844a
hoaibang07 <hoaibang07@gmail.com>
2020-05-05 11:52:37 +0700

    Change repo info

--- os/setup.sh
+++ os/setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-declare -r GITHUB_REPOSITORY="hoaibang07/dotfiles_alrra_linuxmint"
+declare -r GITHUB_REPOSITORY="hoaibang07/linuxmintdotfiles"
 
 declare -r DOTFILES_ORIGIN="git@github.com:$GITHUB_REPOSITORY.git"
 declare -r DOTFILES_TARBALL_URL="https://github.com/$GITHUB_REPOSITORY/tarball/master"
-declare -r DOTFILES_UTILS_URL="https://raw.githubusercontent.com/$GITHUB_REPOSITORY/master/src/os/utils.sh"
+declare -r DOTFILES_UTILS_URL="https://raw.githubusercontent.com/$GITHUB_REPOSITORY/master/os/utils.sh"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-declare dotfilesDirectory="$HOME/projects/dotfiles"
+declare dotfilesDirectory="$HOME/Projects/HoaiNP/dotfiles"
 declare skipQuestions=false
 
 # ----------------------------------------------------------------------
